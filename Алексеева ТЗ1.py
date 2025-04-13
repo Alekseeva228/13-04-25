b28bbd01b1d8939ec9f7ad364480d09ad3f57a55
Alekseeva228 <tumonya79@gmail.com>
2025-04-13 16:25:10 +0700

    Add files via upload

---
+++ Алексеева ТЗ1.py
@@ -0,0 +1,16 @@
+tsvet = input("Введите Логин: ")
+if  tsvet == "Fanta" or tsvet == "450":
+    print("Отлчино!")
+    print("Успешный вход")
+elif  not tsvet == "Simple Dimple" or tsvet == "323":
+    print("Ошибка!")
+    print("Логин неверен")
+elif  tsvet == "Lion" or tsvet == "558":
+    print("Ошибка!")
+    print("Пароль неверен")
+elif not tsvet == "Thailand" or not tsvet == "983":
+    print("Ошибка!")
+    print("Логин и пароль неверный")
+else:
+    print("Добро пожаловать!")
+    
